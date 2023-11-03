3cd72aa23c69f82ffcd98785b3943f1c9015bff4
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2023-11-03 14:43:22 +0000

    Update and delete posts from backend

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from 'express';
-import PostModel from '../models/post';
+import PostModel from '../models/postModel';
 import createHttpError from 'http-errors';
 import mongoose from 'mongoose';
 
@@ -33,8 +33,8 @@ export const getOnePost: RequestHandler = async (req, res, next) => {
 
 // TODO - may have to add ? to these...
 interface CreatePostBody {
-  title: string;
-  text: string;
+  title?: string;
+  text?: string;
 }
 
 export const createPost: RequestHandler<
@@ -50,11 +50,84 @@ export const createPost: RequestHandler<
       throw createHttpError(400, 'Post must have a title!');
     }
 
+    if (!text) {
+      throw createHttpError(400, 'Post must have some text content!');
+    }
+
     const newPost = await PostModel.create({
       title,
       text,
     });
     res.status(201).json(newPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
+interface UpdatePostParams {
+  postId: string;
+}
+
+interface UpdatePostBody {
+  title?: string;
+  text?: string;
+}
+
+export const updatePost: RequestHandler<
+  UpdatePostParams,
+  unknown,
+  UpdatePostBody,
+  unknown
+> = async (req, res, next) => {
+  const postId = req.params.postId;
+  const newPostTitle = req.body.title;
+  const newPostText = req.body.text;
+
+  try {
+    if (!mongoose.isValidObjectId(postId)) {
+      throw createHttpError(400, 'Invalid Post ID');
+    }
+
+    if (!newPostTitle) {
+      throw createHttpError(400, 'Post must have a title!');
+    }
+
+    if (!newPostText) {
+      throw createHttpError(400, 'Post must have some text content!');
+    }
+
+    const post = await PostModel.findById(postId).exec();
+
+    if (!post) {
+      throw createHttpError(404, 'No Post by that ID number!');
+    }
+
+    post.title = newPostTitle;
+    post.text = newPostText;
+
+    const updatedPost = await post.save();
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deletePost: RequestHandler = async (req, res, next) => {
+  const postId = req.params.postId;
+
+  try {
+    if (!mongoose.isValidObjectId(postId)) {
+      throw createHttpError(400, 'Invalid Post ID');
+    }
+
+    const post = await PostModel.findById(postId).exec();
+
+    if (!post) {
+      throw createHttpError(404, 'Post not found');
+    }
+
+    await post.deleteOne();
+    res.sendStatus(204);
   } catch (error) {
     next(error);
   }
--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -4,11 +4,15 @@ import {
   getPosts,
   createPost,
   getOnePost,
+  updatePost,
+  deletePost,
 } from '../controllers/postController';
 const router = express.Router();
 
 router.get('/', getPosts);
-router.post('/', createPost);
 router.get('/:postId', getOnePost);
+router.post('/', createPost);
+router.patch('/:postId', updatePost);
+router.delete('/:postId', deletePost);
 
 export default router;
