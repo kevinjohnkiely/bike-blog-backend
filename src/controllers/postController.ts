71862f8ce300eb0ae9ea9dfd62e18768ea9cb8ea
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2023-11-03 13:23:46 +0000

    HTTP error handling setup

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from 'express';
 import PostModel from '../models/post';
+import createHttpError from 'http-errors';
+import mongoose from 'mongoose';
 
 export const getPosts: RequestHandler = async (req, res, next) => {
   try {
@@ -10,26 +12,49 @@ export const getPosts: RequestHandler = async (req, res, next) => {
   }
 };
 
-export const createPost: RequestHandler = async (req, res, next) => {
-  const { title, text } = req.body;
+export const getOnePost: RequestHandler = async (req, res, next) => {
+  const postId = req.params.postId;
 
   try {
-    const newPost = await PostModel.create({
-      title,
-      text,
-    });
-    res.status(201).json(newPost);
+    if (!mongoose.isValidObjectId(postId)) {
+      throw createHttpError(400, 'Invalid Post ID');
+    }
+
+    const thePost = await PostModel.findById(postId).exec();
+
+    if (!thePost) {
+      throw createHttpError(404, 'Post does not exist!');
+    }
+    res.status(200).json(thePost);
   } catch (error) {
     next(error);
   }
 };
 
-export const getOnePost: RequestHandler = async (req, res, next) => {
-  const postId = req.params.postId;
+// TODO - may have to add ? to these...
+interface CreatePostBody {
+  title: string;
+  text: string;
+}
+
+export const createPost: RequestHandler<
+  unknown,
+  unknown,
+  CreatePostBody,
+  unknown
+> = async (req, res, next) => {
+  const { title, text } = req.body;
 
   try {
-    const thePost = await PostModel.findById(postId).exec();
-    res.status(200).json(thePost);
+    if (!title) {
+      throw createHttpError(400, 'Post must have a title!');
+    }
+
+    const newPost = await PostModel.create({
+      title,
+      text,
+    });
+    res.status(201).json(newPost);
   } catch (error) {
     next(error);
   }
